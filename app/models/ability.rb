3964b63fcd55df9b4bea4622c78ac656749be6a2
Swamy G <swamy@manilla.com>
2012-05-11 16:00:01 -0700

    adding new covers, changing ability, profile updates

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,10 @@ class Ability
     can [:read], :all
     can [:create, :new], Novel if !user.nil?
     can [:delete, :update], Novel do |novel|
-      user.is_owner?(novel)
+      user.is_author?(novel)
     end
     can [:edit], Novel do |novel|
-      user.is_owner?(novel) || user.is_editor?(novel)
+      user.is_author?(novel) || user.is_editor?(novel)
     end
     can [:manage], Chapter do |chapter|
       user.is_author?(chapter.novel) || user.is_editor?(chapter.novel) || user.is_contributor?(chapter.novel) || user.is_moderator?
